e2c87fe138ca8364a923ad9ecf597796fa2a50f4
dazy228 <danilchupak@gmail.com>
2025-04-17 01:59:15 +0300

    Initial commit

---
+++ Django/urls.py
@@ -0,0 +1,9 @@
+from xml.etree.ElementInclude import include
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
+]
